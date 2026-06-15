0bb35781d2ef3ac496e842da6a7291bb6ed9bb86
Mario Zimmermann <mail@zisoft.de>
2026-06-15 19:06:59 +0200

    Initial Commit

---
+++ pass-safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  pass-safari
+//
+//  Created by Mario Zimmermann on 15.06.26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
